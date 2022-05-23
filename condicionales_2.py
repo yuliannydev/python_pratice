6857a1d2c7e33c32aa65d11b2c7c72db00e2ff6e
Yulianny Betancourt <flipsdulce@gmail.com>
2022-05-23 13:35:12 -0500

    :memo: If, elif, else

---
+++ condicionales_2.py
@@ -0,0 +1,8 @@
+numero = int(input("Raking: 'Ingrese un número del 1 al 10' : "))
+
+if numero < 5:
+    print("El servicio es muy malo")
+elif numero >= 8:
+    print("El servicio es muy bueno")
+else:
+    print("Es regular")
